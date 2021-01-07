7d7b3a667918636aaadcd630dff65ce9ae35a171
Aime julien BAUDOT <abaudot@e3r6p20.42.fr>
2021-01-07 16:17:18 +0100

    game over

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -46,12 +46,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (!n)
 		return (0);
-	while (n-- && *s1 == *s2 && *s1)
+	while (*s1 == *s2)
 	{
+		if (!--n || !*s1)
+			return (0);
 		++s1;
 		++s2;
 	}
-	if (!n || !*s1)
-		return (0);
 	return (*(unsigned char*)s1 - *(unsigned char*)s2);
 }
